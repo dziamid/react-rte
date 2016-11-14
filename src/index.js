c4ee7c309dc66d630c814f56386749069c777d03
Dziamid <dziamid@gmail.com>
2016-11-14 20:02:40 +0300

    added material-ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import EditorDemo from './EditorDemo';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import theme from './theme';
 
 document.addEventListener('DOMContentLoaded', () => {
   let rootNode = document.createElement('div');
   document.body.appendChild(rootNode);
   ReactDOM.render(
-    <EditorDemo />,
-    rootNode,
+      <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <EditorDemo />
+      </MuiThemeProvider>,
+      rootNode,
   );
 });
